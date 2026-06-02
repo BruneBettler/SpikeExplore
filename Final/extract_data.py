7763a73019280049dd7c09da60d3bf82ff137c35
BruneBettler <brune.bettler@mail.mcgill.ca>
2026-06-02 16:57:23 +0200

    updates

---
+++ Final/extract_data.py
@@ -0,0 +1,9 @@
+def extract_unit_coords():
+
+
+    return None
+
+
+if __name__ == "__main__":
+    KS4_sorted_path = "/Users/brunebettler/Desktop/NSC/2026_Spring/short_project/viktor08_kilosort4"
+    
